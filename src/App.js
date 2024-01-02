69c6b81c2e24bc2ed2c69bedfdf4b622685c872a
realted <ted.yoo@mail.utoronto.ca>
2024-01-03 05:26:41 +0900

    My first commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Welcome to Ted Yoo's Website</h1>
+        <p>
+          Hi, I'm Ted Yoo. This is my personal website. Feel free to explore!
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
