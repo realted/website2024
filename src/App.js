caa2ac451f02c6cd5f1a19dc37ef847f2247d157
realted <ted.yoo@mail.utoronto.ca>
2024-01-04 07:51:00 +0900

    commit 4

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
 import React from 'react';
+import tedYooImage from './webim1.png';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Welcome to Ted Yoo's Website</h1>
-        <p>
-          Hi, I'm Ted. This is my personal website. Feel free to explore!
-        </p>
-        <a
-          className="App-link"
-          href="https://drive.google.com/file/d/12AK-qRWreXqj3JOBNNbF-U6W69YMEzwO/view?usp=drive_link"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          My Resume
-        </a>
-      </header>
+    <div className = "App">
+      <div className="RowTop">
+        <img src={tedYooImage} className="profile-image" alt="Ted Yoo" />
+        <header className="App-header">
+          <p>Hi! I'm Ted</p>
+        </header>  
+      </div>
+      <div className="Rect1"></div>
     </div>
   );
 }
 
 export default App;
+
+
+
+{/*
+<p className="App-text1">
+Hi, I'm Ted. This is my personal website. Feel free to explore!
+</p>
+<a
+className="App-link"
+href="https://drive.google.com/file/d/12AK-qRWreXqj3JOBNNbF-U6W69YMEzwO/view?usp=drive_link"
+target="_blank"
+rel="noopener noreferrer"
+>
+My Resume
+</a>
+
+*/}
+
+
+
