b4ec7d37a2eec513da8720217ad6f13ea581d7c4
realted <ted.yoo@mail.utoronto.ca>
2024-01-06 15:31:43 +0900

    still working on projects page but good progress

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,10 +1,36 @@
 import React from "react";
+import {Link} from "react-router-dom";
 
+export default function Projects() {
+  const timeline = [
+    {act: "Research Assignment", actDesc: "An ethical analysis of LBAR Games",
+      to: "http://tinyurl.com/57d3p7v8"},
+    {act: "UTEK Hackathon", actDesc: "Creating an effective wind powered car", 
+     to: "http://tinyurl.com/he79sydx"},
+    {act: "RSX ARM Mechanical", actDesc: "Manufacturing Carbon Fibre for extraterrestial rovers", 
+     to: "http://tinyurl.com/5fea93mz"}
+  ];
 
-export default function Projects(){
-    return(
-        <> 
-            <p>projectsdsd</p>
-        </>
-    )
+  return (
+    <>
+    <div className="proj">
+        <header className="y2023">
+            <p>2023</p>
+        </header>
+        
+    {timeline.map((element) => (
+        <Link to={element.to}>
+          <div className = "projBox">
+              <header className = "projDesc">
+                  <p>{element.act}</p>
+              </header>
+              <header className = "projExp">
+                  <p>{element.actDesc}</p>
+              </header>
+          </div>
+        </Link>
+      ))}
+    </div>
+    </>
+  );
 }
