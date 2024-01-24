42646eb121bcbb79461fd4f1a72efaf6a919f4fa
realted <ted.yoo@mail.utoronto.ca>
2024-01-25 08:45:23 +0900

    updated 2024 project

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -2,7 +2,11 @@ import React from "react";
 import {Link} from "react-router-dom";
 
 export default function Projects() {
-  const timeline = [
+  const timeline2024 = [
+    {act: "AR Sunglasses: Medilense", actDesc: "UTRA Hacks biomedical product for ASD Individuals",
+    to: "https://devpost.com/software/medilens?ref_content=my-projects-tab&ref_feature=my_projects", date: "Jan 24"}
+  ];
+  const timeline2023 = [
     {act: "Research Assignment", actDesc: "An ethical analysis of LBAR Games",
       to: "http://tinyurl.com/57d3p7v8", date: "DEC 5"},
     {act: "UTEK Hackathon", actDesc: "Creating an effective wind powered car", 
@@ -19,10 +23,37 @@ export default function Projects() {
     <>
     <div className="proj">
         <header className="y2023">
+            <p>2024</p>
+        </header>
+
+        {timeline2024.map((element, index) => (
+          <div className = "projLayout">
+            <div className = "dayLayout"> 
+              <header className = "projDate">
+                <p>{element.date}</p>
+              </header>
+              <div className = "triangle"> 
+                <div className = "inner-triangle"> </div>
+              </div>
+            </div>
+            <Link key = {index} to={element.to} className = "projBox" target="_blank">
+              <div className = "projBox">
+                  <header className = "projDesc">
+                      <p>{element.act}</p>
+                  </header>
+                  <header className = "projExp">
+                      <p>{element.actDesc}</p>
+                  </header>
+              </div>
+            </Link>
+          </div>
+      ))}
+
+        <header className="y2023">
             <p>2023</p>
         </header>
         
-      {timeline.map((element, index) => (
+      {timeline2023.map((element, index) => (
           <div className = "projLayout">
             <div className = "dayLayout"> 
               <header className = "projDate">
