7a0f89c91668a671636850d8e2baffcd312d333b
realted <ted.yoo@mail.utoronto.ca>
2024-03-04 14:43:51 +0900

    2.0 website homepage

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,42 +1,57 @@
 import React from "react";
-import tedYooImage from './webim1.png';
+import moneyShot from './money.png';
+import iggray from './iggray.png';
+import linkedingray from './linkedingray.png';
+import githubgray from './githubgray.png';
+import { FaGithub, FaInstagram, FaLinkedin } from 'react-icons/fa';
 import {Link} from "react-router-dom";
 
 export default function Home() {
 
   const menuItems = [
-    { to: "/projects", className: "gridBox1", label: "Projects", target: "_self"},
-    { to: "/blogs", className: "gridBox2", label: "Blogs", target: "_self" },
-    { to: "https://github.com/realted", className: "gridBox3", label: "GitHub", target: "_blank" },
-    { to: "https://www.linkedin.com/in/ted-yoo1/", className: "gridBox4", label: "LinkedIn", target: "_blank" },
+    { to: "", className: "descMain", label: "About", target: "_self"},
+    { to: "/blogs", className: "descMain", label: "Blogs", target: "_blank" },
+    { to: "/projects", className: "descMain", label: "Projects", target: "_blank" },
+    { to: "", className: "descMain", label: "Experience", target: "_blank" },
+  ];
+
+  const socialItems = [
+    { to: "https://www.instagram.com/its.teddyyyy/", className: "socials", src: iggray, target: "_blank"},
+    { to: "https://www.linkedin.com/in/ted-yoo1/", className: "socials", src: linkedingray, target: "_blank" },
+    { to: "https://github.com/realted", className: "socials", src: githubgray, target: "_blank" },
   ];
 
   return (
     <div className="App">
-      <div className="RowTop">
-        <img src={tedYooImage} className="profile-image" alt="Ted Yoo" />
-        <header className="App-header">
-          <p>Hi! I'm Ted</p>
-        </header>
-      </div>
-      <div className="Rect1"></div>
+      
+      <div className="flexLR">
 
-      <div className="topDescBox">
-        <header className="desc">
-          <p>
-            My name is Ted Yoo and I am currently a 2nd Year studying Engineering Science at the University of Toronto. I aspire to study and research either robotics, ECE, or Machine Intelligence.
-          </p>
-        </header>
-      </div>
-
-      <div className="gridSelect">
-        {menuItems.map((menuItem, index) => (
-          <Link key={index} to={menuItem.to} className={menuItem.className} target={menuItem.target}>
-            <div className="descBox">
-              <p>{menuItem.label}</p>
-            </div>
-          </Link>
+        <div className="flexColumn">
+          <div className="name">
+            <p>TED <br></br> YOO</p>
+          </div>
+        
+          {menuItems.map((menuItem, index) => (
+            <Link key={index} to={menuItem.to} className={menuItem.className} target={menuItem.target}>
+              <div className="descMain">
+                <p>{menuItem.label}</p>
+              </div>
+            </Link>
         ))}
+        </div>
+        <img src={moneyShot} className="profile-image" alt="Ted Yoo"/>
+      </div>
+
+      <div className="socials">
+        <a href="https://github.com/realted" className = "social-image" target="_blank">
+          <FaGithub />
+        </a>
+        <a href="https://www.instagram.com/its.teddyyyy/" className = "social-image" target="_blank">
+          <FaInstagram />
+        </a>
+        <a href="https://www.linkedin.com/in/ted-yoo1/" className = "social-image" target="_blank">
+          <FaLinkedin />
+        </a>
       </div>
     </div>
   );
