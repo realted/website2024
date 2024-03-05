193a06579f75512f838d51a8fb591f241514656f
realted <ted.yoo@mail.utoronto.ca>
2024-03-05 13:49:48 +0900

    projects page 2.0

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Home from './pages/home';
+import Home from './pages/Home';
 import Projects from './pages/Projects';
 import Blogs from './pages/blogs';
 import About from './pages/about';
