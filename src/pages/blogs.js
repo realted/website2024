040b12e2f1d8da9f60d780b6d59fd7716796d00a
realted <ted.yoo@mail.utoronto.ca>
2024-03-12 23:02:51 -0400

    cleaned up blogs page

--- src/pages/blogs.js
+++ src/pages/blogs.js
@@ -7,31 +7,27 @@ export default function Blogs(){
     const blogContent = [
         {blog: "y2 and running", written: "Written: 1/7 '24", img: blogpic1,
           to: "https://therealtedyoo.substack.com/p/y2-eng-sci-and-running"},
-        {blog: " TBD ", written: "Written: TBD", read: "img: TBD",
-          to: ""},
+      
     ];
     
     return (
 
         <div className="blogs">
             <Navbar />
-            <header className="y2023">
+            <header className="blogTitle">
                 <p>Blogs</p>
             </header>
 
             {blogContent.map((element, index) => (
-                <div className = "blogLayout">
-            
-                    <img src={element.img} className="blogImg" alt="blogImage"/>
-            
-                    <Link  key = {index} to={element.to} className="projDesc" target="_blank">
-                        <header className = "projDesc">
+                <div className = "blogLayout">            
+                    <Link  key = {index} to={element.to} className="blogLayout" target="_blank">
+                        <img src={element.img} className="blogImg" alt="blogImage" style = {{width: '30%'}}/>
+                        <header className = "blogName">
                             <p>{element.blog}</p>
+                            <header className = "blogDesc"> {element.written} </header>
                         </header>
+
                     </Link>
-                    <header className = "blogDesc">
-                        <p> {element.written} </p>
-                    </header>
                 </div>
             ))}
         </div>
